c059f82995fb25dc47e8ab16aa9c03ebda194437
Abhiram Rajeevan <abhiramrajiv14@gmail.com>
2024-12-01 15:37:39 +0530

    Add download option and prettify

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'; // Import FormsModule for template-driven forms
 import { JsonPipe } from '@angular/common';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -16,84 +16,89 @@ export class AppComponent {
   responseData: any = null;
 
   private simulatorData = {
-    "SimulatorSettings": {
-      "NumberOfSimulators": 1,
-      "LogFile": ".\\Simulators.log",
-      "LogLevel": "Information",
-      "StorageRoot": ".",
-      "HttpClientTimeoutInSeconds": 300,
-      "HttpClientMaxConcurrentRequestPerServer": 80
+    SimulatorSettings: {
+      NumberOfSimulators: 1,
+      LogFile: '.\\Simulators.log',
+      LogLevel: 'Information',
+      StorageRoot: '.',
+      HttpClientTimeoutInSeconds: 300,
+      HttpClientMaxConcurrentRequestPerServer: 80,
     },
-    "DownloadOptions": {
-      "DownloadAgentInstaller": false,
-      "DownloadAgentConfigFile": false,
-      "_comment1": "Choose one of the available modes (Disabled, ProcessWithoutDownload, ProcessWithDownload) in the AppCatalogMode field. Disabled to not support AppCatalog, and others to support with/without download.",
-      "AppCatalogMode": "Disabled"
+    DownloadOptions: {
+      DownloadAgentInstaller: false,
+      DownloadAgentConfigFile: false,
+      _comment1:
+        'Choose one of the available modes (Disabled, ProcessWithoutDownload, ProcessWithDownload) in the AppCatalogMode field. Disabled to not support AppCatalog, and others to support with/without download.',
+      AppCatalogMode: 'Disabled',
     },
-    "CustomDataConfig": {
-      "CustomData": {
-        "Key1": "Value1",
-        "Key2": "Value2"
-      }
-    },
-    "DeviceConfig": {
-      "Model": "Surface Pro 9",
-      "Manufacturer": "Microsoft",
-      "Resolution": {
-        "Width": 2880,
-        "Height": 1920
+    CustomDataConfig: {
+      CustomData: {
+        Key1: 'Value1',
+        Key2: 'Value2',
       },
-      "OSLanguage": "en-CA",
-      "DeviceName": "WindowsModern-Simulator",
-      "IPRange": {
-        "MinIP": "192.168.0.0",
-        "MaxIP": "192.168.0.254"
+    },
+    DeviceConfig: {
+      Model: 'Surface Pro 9',
+      Manufacturer: 'Microsoft',
+      Resolution: {
+        Width: 2880,
+        Height: 1920,
       },
-      "CommercializationOperator": "SOTI SIM CARRIER",
-      "Programs": [
+      OSLanguage: 'en-CA',
+      DeviceName: 'WindowsModern-Simulator',
+      IPRange: {
+        MinIP: '192.168.0.0',
+        MaxIP: '192.168.0.254',
+      },
+      CommercializationOperator: 'SOTI SIM CARRIER',
+      Programs: [
         {
-          "AppId": "Google Chrome",
-          "Name": "Google Chrome",
-          "Version": "127.0.6533.73",
-          "SizeInBytes": 1048576
+          AppId: 'Google Chrome',
+          Name: 'Google Chrome',
+          Version: '127.0.6533.73',
+          SizeInBytes: 1048576,
         },
         {
-          "AppId": "Microsoft Edge",
-          "Name": "Microsoft Edge",
-          "Version": "127.0.2651.74",
-          "SizeInBytes": 2097152
-        }
+          AppId: 'Microsoft Edge',
+          Name: 'Microsoft Edge',
+          Version: '127.0.2651.74',
+          SizeInBytes: 2097152,
+        },
       ],
-      "AgentVersion": "2025.1.0.0",
-      "TrustedRootCertificatesData": [ ""]
+      AgentVersion: '2025.1.0.0',
+      TrustedRootCertificatesData: [''],
     },
-    "EnrollmentConfig": {
-      "DsAddresses": [
+    EnrollmentConfig: {
+      DsAddresses: [
         {
-          "Host": "ec2-34-238-24-195.compute-1.amazonaws.com"
-        }
+          Host: 'ec2-34-238-24-195.compute-1.amazonaws.com',
+        },
       ],
-      "EmailAddress": "user1@bdd.soti",
-      "Credentials": {
-        "Type": "ProvisioningPackage",
-        "ProvisioningPackageCredentialsConfig": {
-          "ServiceDiscoveryAddress": "",
-          "EnrollmentCertificateData": "",
-          "EnrollmentCertificatePassword": ""
-        }
-      }
-    }
+      EmailAddress: 'user1@bdd.soti',
+      Credentials: {
+        Type: 'ProvisioningPackage',
+        ProvisioningPackageCredentialsConfig: {
+          ServiceDiscoveryAddress: '',
+          EnrollmentCertificateData: '',
+          EnrollmentCertificatePassword: '',
+        },
+      },
+    },
   };
 
   // Method to get simulator data
   getSimulatorData() {
-    if(this.responseData != null)
-    {
-      this.simulatorData.EnrollmentConfig.DsAddresses[0].Host = this.responseData.Host;
-      this.simulatorData.DeviceConfig.TrustedRootCertificatesData[0] = this.responseData.EncodedCert;
-      this.simulatorData.EnrollmentConfig.Credentials.ProvisioningPackageCredentialsConfig.ServiceDiscoveryAddress = this.responseData.DiscoveryUrl;
-      this.simulatorData.EnrollmentConfig.Credentials.ProvisioningPackageCredentialsConfig.EnrollmentCertificateData = this.responseData.PfxCertBlob;
-      this.simulatorData.EnrollmentConfig.Credentials.ProvisioningPackageCredentialsConfig.EnrollmentCertificatePassword = this.responseData.PfxCertPassword;
+    if (this.responseData != null) {
+      this.simulatorData.EnrollmentConfig.DsAddresses[0].Host =
+        this.responseData.Host;
+      this.simulatorData.DeviceConfig.TrustedRootCertificatesData[0] =
+        this.responseData.EncodedCert;
+      this.simulatorData.EnrollmentConfig.Credentials.ProvisioningPackageCredentialsConfig.ServiceDiscoveryAddress =
+        this.responseData.DiscoveryUrl;
+      this.simulatorData.EnrollmentConfig.Credentials.ProvisioningPackageCredentialsConfig.EnrollmentCertificateData =
+        this.responseData.PfxCertBlob;
+      this.simulatorData.EnrollmentConfig.Credentials.ProvisioningPackageCredentialsConfig.EnrollmentCertificatePassword =
+        this.responseData.PfxCertPassword;
     }
     return this.simulatorData;
   }
@@ -106,13 +111,13 @@ export class AppComponent {
     } else {
       alert('Invalid file type. Please select a PPKG file.');
       this.selectedFile = null;
-      event.target.value = ''
+      event.target.value = '';
     }
   }
 
   isFileTypeValid(file: File): boolean {
     const allowedExtension = '.ppkg';
-    return file.name.toLowerCase().endsWith(allowedExtension); 
+    return file.name.toLowerCase().endsWith(allowedExtension);
   }
 
   onSubmit(): void {
@@ -124,15 +129,20 @@ export class AppComponent {
     formData.append('file', this.selectedFile, this.selectedFile.name);
 
     // Send file to Go server
-    this.http.post('https://provisioning-details-extractor-vercel-backend.vercel.app/upload', formData).subscribe(
-      (response) => {
-        console.log('File uploaded successfully', response);
-        this.responseData = response;
-      },
-      (error) => {
-        console.error('Error uploading file', error);
-      }
-    );
+    this.http
+      .post(
+        'https://provisioning-details-extractor-vercel-backend.vercel.app/upload',
+        formData
+      )
+      .subscribe(
+        (response) => {
+          console.log('File uploaded successfully', response);
+          this.responseData = response;
+        },
+        (error) => {
+          console.error('Error uploading file', error);
+        }
+      );
   }
 
   copyToClipboard(content: any): void {
@@ -146,7 +156,16 @@ export class AppComponent {
     alert('Content copied to clipboard!');
   }
 
-  resetData() : void {
-    this.responseData = null
+  resetData(): void {
+    this.responseData = null;
+  }
+  downloadJson(): void {
+    const jsonContent = JSON.stringify(this.getSimulatorData(), null, 2);
+    const blob = new Blob([jsonContent], { type: 'application/json' });
+
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'appsettings.json';
+    link.click();
   }
 }
